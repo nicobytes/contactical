eef93f29925da0ee5f3d53a4df107dbd53c2f451
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2016-02-10 17:45:11 -0500

    Write 2e2 test

--- tests/e2e/app.spec.js
+++ tests/e2e/app.spec.js
@@ -1,12 +1,36 @@
 describe('end to end tests', function() {
-	it('should have contact', function(done){
-		browser.get('http://localhost:8080');
 
+	it('should add a contact and not throw an error',function(done){
+		browser.get('http://localhost:8080');
+		browser.sleep(1000);
+		element(by.model('add.newContact.name')).sendKeys('ivan camilo');
+		element(by.model('add.newContact.email')).sendKeys('davos@onionknig.ht');
+		element(by.id('submitNewContact')).click();	
 		element.all(by.repeater('contact in list.contacts'))
-		.then(function( contacts ){
-			var text = contacts[0].getText();
-			expect(text).toEqual('nicolas molina N');
+		.then(function(contacts){
+			expect(contacts.length).toEqual(2);
+			done();	
+		});
+	});
+
+	it('should display the contacts name correctly', function(done) {		
+		element.all(by.repeater('contact in list.contacts'))
+		.then(function(contacts){
+			var newest = contacts[contacts.length-1];
+			var name = newest.element(by.tagName('h4')).element(by.className('contact-name'));
+			expect(name.getText()).toEqual("Ivan Camilo");
 			done();
 		});
-	})
+  });
+
+  it('should display the contacts avatar properly', function(done) {		
+		element.all(by.repeater('contact in list.contacts'))
+		.then(function(contacts){
+			var newest = contacts[contacts.length-1];
+			var avatar = newest.element(by.tagName('h4')).element(by.className('avatar'));
+			expect(avatar.getText()).toEqual("I");			
+		});
+		done();
+  });
+
 });
